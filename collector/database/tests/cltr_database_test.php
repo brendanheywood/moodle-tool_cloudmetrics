10e91d33a89dc8d82ddc2baacc6f46d28ba02670
Jason den Dulk <jasondendulk@catalyst-au.net>
2022-06-01 18:04:16 +1000

    Fix unit test

--- collector/database/tests/cltr_database_test.php
+++ collector/database/tests/cltr_database_test.php
@@ -83,7 +83,9 @@ class cltr_database_test extends \tool_cloudmetrics\metric_testcase {
         $rec = $DB->get_records(lib::TABLE);
         $this->assertEquals(0, count($rec));
 
-        $collector->record_metric($stub->generate_metric_item(0, 0));
+        $time = 100;
+        $collector->record_metric($stub->generate_metric_item(0, $time));
+        $time += 10;
 
         // Should have one metric of value 1.
         $rec = array_values($DB->get_records(lib::TABLE));
@@ -91,7 +93,8 @@ class cltr_database_test extends \tool_cloudmetrics\metric_testcase {
         $this->assertEquals('mock', $rec[0]->name);
         $this->assertEquals('1', $rec[0]->value);
 
-        $collector->record_metric($stub->generate_metric_item(0, 0));
+        $collector->record_metric($stub->generate_metric_item(0, $time));
+        $time += 10;
 
         // Should have two metrics of values 1 & 2.
         $rec = array_values($DB->get_records(lib::TABLE, null, 'time ASC'));
@@ -107,8 +110,9 @@ class cltr_database_test extends \tool_cloudmetrics\metric_testcase {
         $this->assertEquals('1', $rec[0]->value);
         $this->assertEquals('2', $rec[1]->value);
 
-        $collector->record_metric($stub->generate_metric_item(0, 0));
-        $collector->record_metric($stub->generate_metric_item(0, 0));
+        $collector->record_metric($stub->generate_metric_item(0, $time));
+        $time += 10;
+        $collector->record_metric($stub->generate_metric_item(0, $time));
 
         // Should have four metrics of values 1, 2, 3 & 1.
         $rec = array_values($collector->get_metrics('mock'));

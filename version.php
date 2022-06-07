551da3e55b34dfb2db3c94bc70527872c731130f
Brendan Heywood <brendan@catalyst-au.net>
2022-06-07 12:13:29 +1000

    Improved the collector checks (#103)

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2022060100;
-$plugin->release = 2022060100;
+$plugin->version = 2022060700;
+$plugin->release = 2022060700;
 
 $plugin->requires = 2017051500;    // Our lowest supported Moodle (3.3.0).
 

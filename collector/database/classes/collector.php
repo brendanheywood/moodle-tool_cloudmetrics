a22c2bcb779395b0814e82a0ff16f9219192c650
marcghaly <marc-alexandreghaly@catalyst-ca.net>
2022-09-04 19:44:21 -0400

    Issue #155: use of db api cast fix (#156)

--- collector/database/classes/collector.php
+++ collector/database/classes/collector.php
@@ -143,7 +143,7 @@ class collector extends base {
         } else {
             $metricselect = '';
             foreach ($params as $param) {
-                $metricselect .= "AVG(CASE name WHEN '$param' THEN CAST(value AS INT) END) $param,";
+                $metricselect .= "AVG(CASE name WHEN '$param' THEN" . $DB->sql_cast_char2int('value', true) . "END) $param,";
             }
             $metricselect = rtrim($metricselect, ',');
             $sql = "SELECT $incrementstart,
